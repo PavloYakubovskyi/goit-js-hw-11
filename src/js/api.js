646a1f7c97e4f78432c644f3650c128dfc923808
Pavlo Yakubovskyi <pavloyakubovskyi@gmail.com>
2023-08-22 23:03:23 +0200

    added all

---
+++ src/js/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { API_KEY, BASE_URL, PER_PAGE } from './constans';
+
+export async function getImages(page = 1, value) {
+  axios.defaults.baseURL = BASE_URL;
+  axios.defaults.params = {
+    key: API_KEY,
+    q: value,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: PER_PAGE,
+    page: page,
+  };
+  // return await axios.get(BASE_URL);
+  const { data } = await axios.get();
+  return data;
+}
---
+++ src/js/constans.js
@@ -0,0 +1,3 @@
+export const API_KEY = '38952282-40725538619d219cb8ed057cd';
+export const BASE_URL = `https://pixabay.com/api/`;
+export const PER_PAGE = 40;
